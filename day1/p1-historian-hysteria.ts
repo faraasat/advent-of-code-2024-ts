e11eeef4015e8e55d56807343353c2e723ee5aa0
Farasat Ali <fali@digital-dividend.com>
2024-12-04 18:09:42 +0500

    day 1 part 1 done

--- day1/p1-historian-hysteria.ts
+++ day1/p1-historian-hysteria.ts
@@ -1,1 +1,20 @@
-console.log("here")
+import { readFileAsString } from "../common.js";
+
+(async () => {
+  const { data, error } = await readFileAsString("../inputs/input1.txt");
+
+  if (error) throw error;
+
+  let sum = 0;
+  const cols = data?.split("\n");
+  const [col1, col2] = await Promise.all([
+    cols?.map((col) => parseInt(col?.split("   ")[0]))?.sort((a, b) => a - b)!,
+    cols?.map((col) => parseInt(col?.split("   ")[1]))?.sort((a, b) => a - b)!,
+  ]);
+
+  for (let i = 0; i < col1.length; i++) {
+    sum += Math.abs(col1[i] - col2[i]);
+  }
+
+  console.log(sum);
+})();
