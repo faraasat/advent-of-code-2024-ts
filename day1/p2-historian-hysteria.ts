8d92e84ad324d276553732fbf101829b2b793053
Farasat Ali <fali@digital-dividend.com>
2024-12-04 18:09:54 +0500

    day 1 part 2 done

---
+++ day1/p2-historian-hysteria.ts
@@ -0,0 +1,26 @@
+import { readFileAsString } from "../common.js";
+
+(async () => {
+  const { data, error } = await readFileAsString("../inputs/input1.txt");
+
+  if (error) throw error;
+
+  let sum = 0;
+
+  const getRegex = (number: number) => new RegExp(`\\b${number}\\b`, "g");
+
+  const cols = data?.split("\n");
+  const [col1, col2] = await Promise.all([
+    cols?.map((col) => Number(col?.split("   ")[0]))!,
+    cols?.map((col) => col?.split("   ")[1]).join(" ")!,
+  ]);
+
+  for (let i = 0; i < col1.length; i++) {
+    const currentNumber = col1[i];
+    const occurrence = col2.match(getRegex(currentNumber))?.length;
+
+    sum += occurrence ? occurrence * currentNumber : 0;
+  }
+
+  console.log(sum);
+})();
