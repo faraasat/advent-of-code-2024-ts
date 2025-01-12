f5a047998412095ddf6ae6468b267de99d045e57
Farasat Ali <farasataliazeemi1@gmail.com>
2025-01-12 12:43:36 +0500

    misc

--- main.ts
+++ main.ts
@@ -4,6 +4,10 @@ import { p1RedNoseReport } from "./day2/p1-red-nose-report.ts";
 import { p2RedNoseReport } from "./day2/p2-red-nose-report.ts";
 import { p1MullItOver } from "./day3/p1-mull-it-over.ts";
 import { p2MullItOver } from "./day3/p2-mull-it-over.ts";
+import { p1CeresSearch } from "./day4/p1-ceres-search.ts";
+import { p2CeresSearch } from "./day4/p2-ceres-search.ts";
+import { p1PrintQueue } from "./day5/p1-print-queue.ts";
+import { p2PrintQueue } from "./day5/p2-print-queue.ts";
 
 const logger = (
   day: number,
@@ -39,11 +43,27 @@ const logger = (
   await p2RedNoseReport();
   logger(2, 2, "Red-Nose Report", false);
 
-  logger(2, 2, "Mull It Over");
+  logger(3, 1, "Mull It Over");
   await p1MullItOver();
-  logger(2, 2, "Mull It Over", false);
+  logger(3, 1, "Mull It Over", false);
 
-  logger(2, 2, "Mull It Over");
+  logger(3, 2, "Mull It Over");
   await p2MullItOver();
-  logger(2, 2, "Mull It Over", false);
+  logger(3, 2, "Mull It Over", false);
+
+  logger(4, 1, "Ceres Search");
+  await p1CeresSearch();
+  logger(4, 1, "Ceres Search", false);
+
+  logger(4, 2, "Ceres Search");
+  await p2CeresSearch();
+  logger(4, 2, "Ceres Search", false);
+
+  logger(5, 1, "Print Queue");
+  await p1PrintQueue();
+  logger(5, 1, "Print Queue", false);
+
+  logger(5, 2, "Print Queue");
+  await p2PrintQueue();
+  logger(5, 2, "Print Queue", false);
 })();
